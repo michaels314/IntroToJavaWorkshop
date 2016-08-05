35ca615ce899108842a8c7f1ff026437e52ed5e8
Skylar <gerings17@bishops.com>
2016-08-05 14:54:37 -0700

    5th Day Perfect

--- src/MichaelsMessages.java
+++ src/MichaelsMessages.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	JOptionPane.showMessageDialog(null, "You're awesome because EVERYTHING IS AWESOME!!!!!!");
 JOptionPane.showMessageDialog(null, "I'm awesome BECAUSE EVERYTHING IS AWESOME!!!");
 JOptionPane.showMessageDialog(null, "They're awesome because EVERYTHING IS AWESOME!!!");
-Ω
+
 
 
 
